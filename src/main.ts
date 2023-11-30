718ddb58e862e7306955969e38d3d5d173b447e0
Julian Waller <git@julusian.co.uk>
2023-11-30 20:50:36 +0000

    feat: update to support companion 3.x

--- src/main.ts
+++ src/main.ts
@@ -15,11 +15,12 @@
  *
  */
 
-import clone from 'git-clone/promise'
 import fs from 'fs'
-import { exec, execSync } from 'child_process'
+import { execSync } from 'child_process'
 import axios from 'axios'
 import os from 'os'
+import path from 'path'
+import type { ModuleManifest } from '@companion-module/base'
 
 const pkg = require('../package.json')
 
@@ -30,28 +31,22 @@ export interface CompanionModulesReport {
 }
 
 export interface CompanionModule {
-	name: string
 	version: string
+	// path: string
+	name: string
+	url: string
+	help_url: string // New
 	api_version: string
-	manufacturer: string | string[]
-	product: string | string[]
-	author: string | string[]
+	keywords: string[]
+	manufacturer: string[] | string
+	product: /* string | */ string[]
+	shortname: string
+	author: /* string | */ string[]
 }
 
 let errors: [string, any][] = []
 
-let packages: {
-	version: string
-	path: string
-	name: string
-	url: string
-	api_version: string
-	keywords: string[]
-	manufacturer: string[] | string
-	product: string | string[]
-	shortname: string
-	author: string | string[]
-}[] = []
+let packages: CompanionModule[] = []
 
 const main = async () => {
 	console.log('[1] Starting', pkg.version)
@@ -59,85 +54,128 @@ const main = async () => {
 	// delete folder recursive tmp/companion if it exists
 	if (fs.existsSync('tmp/companion')) {
 		console.log('[1.5] Deleting tmp/companion')
-		fs.rmdirSync('tmp/companion', { recursive: true })
+		fs.rmSync('tmp/companion', { recursive: true })
 	}
 
 	console.log('[2] Cloning companion')
 	try {
-		const res = await clone('https://github.com/bitfocus/companion', './tmp/companion')
+		execSync('git clone https://github.com/bitfocus/companion ./tmp/companion --recursive')
 	} catch (err) {
 		console.log('[2] Error cloning companion')
 		console.log(err)
 		//errors.push(["clone", err]);
 	}
 
-	// Run yarn in the companion folder
-	console.log('[3] Running yarn')
-	try {
-		execSync('yarn --frozen-lockfile --prod', { cwd: './tmp/companion' })
-	} catch (err) {
-		console.log('[3] Error running yarn')
-		console.log(err)
-		errors.push(['yarn', err])
+	// create a list of modern packages
+	console.log('[3] Reading bundled modules')
+	const bundledModulesRoot = './tmp/companion/bundled-modules'
+	const bundledModules = fs.readdirSync(bundledModulesRoot)
+	for (const moduleFolder of bundledModules) {
+		const moduleManifestPath = path.join(bundledModulesRoot, moduleFolder, 'companion/manifest.json')
+		try {
+			if (!fs.existsSync(moduleManifestPath)) continue
+
+			const moduleManifest: ModuleManifest = JSON.parse(fs.readFileSync(moduleManifestPath, 'utf8'))
+
+			packages.push({
+				version: moduleManifest.version,
+				name: moduleManifest.name,
+				url: moduleManifest.repository,
+				help_url: `https://github.com/bitfocus/companion-bundled-modules/blob/main/${moduleManifest.name}/companion/HELP.md`,
+				api_version: moduleManifest.runtime.apiVersion,
+				keywords: moduleManifest.keywords,
+				manufacturer: moduleManifest.manufacturer,
+				product: moduleManifest.products,
+				shortname: moduleManifest.shortname,
+				author: moduleManifest.maintainers.map((mt) => (mt.email ? `${mt.name} <${mt.email}>` : mt.name)),
+			})
+		} catch (err) {
+			console.log('[3] Error reading manifest.json', moduleManifestPath)
+			console.log(err)
+			errors.push(['manifest.json', err])
+		}
 	}
 
-	// read package.json in companion folder
-	console.log('[4] Reading package.json')
-	let package_json: any = {}
-	try {
-		package_json = JSON.parse(fs.readFileSync('./tmp/companion/package.json', 'utf8'))
-	} catch (err) {
-		console.log('[4] Error reading package.json')
-		console.log(err)
-		errors.push(['package.json', err])
-	}
+	if (fs.existsSync('./tmp/companion/module-legacy')) {
+		// Run yarn in the companion folder
+		console.log('[4] Running yarn')
+		try {
+			execSync('yarn --frozen-lockfile --prod', { cwd: './tmp/companion/module-legacy' })
+		} catch (err) {
+			console.log('[4] Error running yarn')
+			console.log(err)
+			errors.push(['yarn', err])
+		}
 
-	// create a list of all packages
-	if (package_json?.dependencies) {
-		Object.keys(package_json.dependencies).forEach((key) => {
-			if (key.startsWith('companion-module-')) {
-				// read "./tmp/companion/node_modules/" + key + "/package.json"
-				let sub_package_json: any = {}
-				try {
-					sub_package_json = JSON.parse(
-						fs.readFileSync('./tmp/companion/node_modules/' + key + '/package.json', 'utf8')
-					)
-				} catch (err) {
-					console.log('[4] Error reading sub package.json', './tmp/companion/node_modules/' + key + '/package.json')
-					console.log(err)
-					errors.push(['sub-package.json', err])
+		// read package.json in companion folder
+		console.log('[5] Reading package.json')
+		let package_json: any = {}
+		try {
+			package_json = JSON.parse(fs.readFileSync('./tmp/companion/module-legacy/package.json', 'utf8'))
+		} catch (err) {
+			console.log('[5] Error reading package.json')
+			console.log(err)
+			errors.push(['package.json', err])
+		}
+
+		// create a list of all packages
+		if (package_json?.dependencies) {
+			Object.keys(package_json.dependencies).forEach((key) => {
+				if (key.startsWith('companion-module-')) {
+					// read "./tmp/companion/node_modules/" + key + "/package.json"
+					let sub_package_json: any = {}
+					try {
+						sub_package_json = JSON.parse(
+							fs.readFileSync('./tmp/companion/module-legacy/node_modules/' + key + '/package.json', 'utf8')
+						)
+					} catch (err) {
+						console.log(
+							'[5] Error reading sub package.json',
+							'./tmp/companion/module-legacy/node_modules/' + key + '/package.json'
+						)
+						console.log(err)
+						errors.push(['sub-package.json', err])
+					}
+
+					packages.push({
+						version: package_json.dependencies[key].split(/#v?/)[1],
+						help_url: `https://github.com/bitfocus/${key}/blob/${package_json.dependencies[key].split(/#/)[1]}/HELP.md`,
+						name: key.replace('companion-module-', ''),
+						url: package_json.dependencies[key],
+						api_version: sub_package_json.api_version,
+						keywords: sub_package_json.keywords,
+						manufacturer: sub_package_json.manufacturer,
+						product: Array.isArray(sub_package_json.product) ? sub_package_json.product : [sub_package_json.product],
+						shortname: sub_package_json.shortname,
+						author: [sub_package_json.author],
+					})
 				}
-
-				packages.push({
-					version: package_json.dependencies[key].split(/#v?/)[1],
-					path: './tmp/companion/node_modules/' + key,
-					name: key.replace('companion-module-', ''),
-					url: package_json.dependencies[key],
-					api_version: sub_package_json.api_version,
-					keywords: sub_package_json.keywords,
-					manufacturer: sub_package_json.manufacturer,
-					product: sub_package_json.product,
-					shortname: sub_package_json.shortname,
-					author: sub_package_json.author,
-				})
-			}
-		})
+			})
+		}
+	} else {
+		console.log('[4] Skipping module-legacy')
 	}
 
 	if (!errors.length) {
-		console.log('[5] Writing output')
+		console.log('[6] Writing output')
 		let output: CompanionModulesReport = {
 			generated_at: new Date(),
 			generated_by: 'companion-modules-generator @ ' + os.hostname(),
 			modules: packages,
 		}
 
-		// Use axios to POST request output to https://api.bitfocus.io/v1/companion/modules
-		axios.post('https://api.bitfocus.io/v1/webhooks/modules', output, {
-			headers: {
-				secret: process.env.MODULE_CRON_SECRET || 'missing cron secret',
-			},
-		})
+		if (process.env.DEBUG_WRITE_TO_FILE) {
+			fs.writeFileSync('test.json', JSON.stringify(output, undefined, 4))
+			console.log('File written')
+		} else {
+			// Use axios to POST request output to https://api.bitfocus.io/v1/companion/modules
+			await axios.post('https://api.bitfocus.io/v1/webhooks/modules', output, {
+				headers: {
+					secret: process.env.MODULE_CRON_SECRET || 'missing cron secret',
+				},
+			})
+			console.log('Data submitted')
+		}
 	} else {
 		console.log('Failed')
 		console.error('We have errors')
